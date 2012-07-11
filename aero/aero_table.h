a299f32ecfcdbbceec7ed39b5a8c655ed30c4c79
Henry Hallam <henry@cosmogia.com>
2012-07-11 16:24:03 -0700

    Added hooks for attsim to look up torques in the aero table
    See aero/aero_table.h

--- aero/aero_table.h
+++ aero/aero_table.h
@@ -17,10 +17,16 @@ typedef struct {
   aero_table_output_t *table;
 } aero_table_t;
 
+// attsim should just care about these two functions:
+int open_table(const char *filename);
+int get_aero_torque(xyz_t *torque, xyz_t *velocity_body, double atmosph_density);
 
+
+
+  
 int write_aero_table_entry(FILE *f, double a, double b, int c, aero_table_output_t *result); 
 
-int load_aero_table(aero_table_t *t, char *filename);
+int load_aero_table(aero_table_t *t, const char *filename);
 void close_aero_table(aero_table_t *t);
 
 int lookup_aero_table(aero_table_output_t *out, aero_table_t *t, double a, double b, int c);
